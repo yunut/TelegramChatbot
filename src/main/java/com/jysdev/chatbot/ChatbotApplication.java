38f86b745325bd73bc9ca52e1186f9779dda5169
yunut <somewheres@nate.com>
2022-09-23 21:23:33 +0900

    Change the development environment to spring boot
    separate the properties value
    
    dotenv lib remove

--- src/main/java/com/jysdev/chatbot/ChatbotApplication.java
+++ src/main/java/com/jysdev/chatbot/ChatbotApplication.java
@@ -1,28 +1,14 @@
 package com.jysdev.chatbot;
 
-import com.jysdev.chatbot.config.ConfigInitialize;
-import com.jysdev.chatbot.handler.ChatbotHandler;
-import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@SpringBootApplication
 public class ChatbotApplication {
 
     public static void main(String[] args) {
-
-        // config init
-        ConfigInitialize configInitialize = new ConfigInitialize();
-        configInitialize.init();
-
-        // chatbot init
-        try {
-            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
-            telegramBotsApi.registerBot(new ChatbotHandler(configInitialize.getEnv("userName"), configInitialize.getEnv("token")));
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
-        }
-
-
+        SpringApplication.run(ChatbotApplication.class);
     }
 
 }
--- src/main/java/com/jysdev/chatbot/handler/ChatbotHandler.java
+++ src/main/java/com/jysdev/chatbot/handler/ChatbotHandler.java
@@ -1,18 +1,20 @@
 package com.jysdev.chatbot.handler;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
+@Component
 public class ChatbotHandler extends TelegramLongPollingBot {
 
     private final String userName;
     private final String token;
 
-    public ChatbotHandler(String userName, String token) {
+    public ChatbotHandler(@Value("${telegram.chatbot.userName}") String userName, @Value("${telegram.chatbot.token}") String token) {
         this.userName = userName;
         this.token = token;
     }
-
     @Override
     public String getBotUsername() {
         return userName;
@@ -25,6 +27,9 @@ public class ChatbotHandler extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
+
+        //TODO 명령어 입력에 따른 api 로직 분기
         System.out.println(update.getMessage().getText());
+
     }
 }
